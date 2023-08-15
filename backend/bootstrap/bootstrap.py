436840a1ea88fb66b5eef717b7a1b97d4cb495e3
Will Seagar <w.seagar@gmail.com>
2023-08-15 14:54:16 +1200

    Delete custom python backed

--- backend/bootstrap/bootstrap.py
+++
@@ -1,8 +0,0 @@
-from imaginairy import imagine, imagine_image_files, ImaginePrompt, WeightedPrompt, generate_caption
-
-prompts = [
-    ImaginePrompt("a scenic landscape", seed=1, upscale=True, fix_faces=True, steps=1),
-]
-result = next(imagine(prompts, precision="full"))
-
-caption = generate_caption(result.img)
